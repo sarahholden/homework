18ab871a7facaa2e6403cf2e4e0636a555dc1c36
sarahholden <sarahbeth.holden@gmail.com>
2016-04-05 21:04:01 -0700

    Added temp conveter

--- select_fun/js/main.js
+++ select_fun/js/main.js
@@ -7,7 +7,7 @@
 // 1. Go to style.css and add a class called fun. Style that class with your choice of background-color and text color.
 
 // 2. Using jQuery, add the fun class to the elements with the class .box
-
+$('.box').addClass('fun');
 // 3. When #box2 is clicked on
 	// a) Remove the crazy class from all the items with a class of .box
 	// b) Add the crazy class to the h1
