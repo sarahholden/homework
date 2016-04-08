585fa35ecea3cb7f036f41a92f04c85f78498a7d
sarahholden <sarahbeth.holden@gmail.com>
2016-04-07 18:45:46 -0700

    Added wireframes

--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -1,1 +1,7 @@
+// When the user clicks the #fahrenheit_to_celsius button
+	// Get the value from #temperature and store in variable fahrenheit
+	// Convert that value to a number
 
+	// Convert to celsius and store in variable celsius
+
+	// Display in #result
